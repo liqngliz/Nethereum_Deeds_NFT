472c5d03e5e75f17f3454ba2dfe2e813cd4e7361
Li, Ang <angli2@kpmg.com>
2023-12-07 11:39:48 +0100

    Deeds inventory now returns token ID and Doc hash

--- Nethreum.Deeds.NFT/Controls/DeedsInventory.cs
+++ Nethreum.Deeds.NFT/Controls/DeedsInventory.cs
@@ -1,10 +1,35 @@
-﻿namespace Nethreum.Deeds.NFT.Controls
+﻿using Nethereum_Deeds_NFT.Contracts.Claims;
+using Nethereum_Deeds_NFT.Contracts.Deeds;
+using System.Numerics;
+
+namespace Nethreum.Deeds.NFT.Controls
 {
-    public class DeedsInventory : IInventory<Tuple<ulong, string>>
-    {
-        List<Tuple<ulong, string>> IInventory<Tuple<ulong, string>>.Inventory()
+    public class DeedsInventory : IInventory<Deed>
+    {   
+        readonly DeedsService _serviceDeeds;
+        readonly ClaimsService _serviceClaims;
+
+        public DeedsInventory(DeedsService deedsService, ClaimsService claimsService) 
+        { 
+            _serviceDeeds = deedsService;
+            _serviceClaims = claimsService;
+        }
+
+        public List<Deed> Inventory()
         {
-            throw new NotImplementedException();
+            var maxTokenNumber = (ulong)_serviceDeeds.GetLastTokenIdQueryAsync().Result;
+            var result = new List<Deed>();
+            for (ulong i = 1; i <= maxTokenNumber; i++) 
+            {
+                BigInteger iAsBigInt = i;
+                var hash = _serviceClaims.GetClaimQueryAsync(iAsBigInt).Result;
+                Deed tuple = new(i, hash);
+                result.Add(tuple);
+            }
+
+            return result;
         }
     }
+
+    public record Deed(ulong TokenId, string DocumentHash);
 }
--- Nethreum.Deeds.NFT/Program.cs
+++ Nethreum.Deeds.NFT/Program.cs
@@ -1,5 +1,8 @@
 using Nethereum.Web3;
+using Nethereum_Deeds_NFT.Contracts.Claims;
 using Nethereum_Deeds_NFT.Contracts.Deeds;
+using Nethreum.Deeds.NFT.Controls;
+using Newtonsoft.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,6 +26,10 @@ var web3 = new Web3(url);
 var contractAddressDeeds = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 var serviceDeeds = new DeedsService(web3, contractAddressDeeds);
 
-app.MapGet("/", async () => (await serviceDeeds.GetLastTokenIdQueryAsync()).ToString());
+var contractAddressClaims = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+var serviceClaims = new ClaimsService(web3, contractAddressClaims);
+var inventory = new DeedsInventory(serviceDeeds, serviceClaims);
+
+app.MapGet("/", () => JsonConvert.SerializeObject(inventory.Inventory()));
 
 app.Run();
